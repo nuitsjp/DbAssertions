607599401646bc4645c5f53dd54095e3cef7a1c5
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2021-06-28 13:23:16 +0900

    Add tests to CompareResultTest.

---
+++ DbAssertions.Test/CompareResultTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace DbAssertions.Test
+{
+    public class CompareResultTest
+    {
+        public class ResultsWillBeManaged
+        {
+            [Fact]
+            public void When_error_not_registered()
+            {
+                CompareResult compareResult = new();
+                compareResult.HasMismatched.Should().BeFalse();
+                compareResult.MismatchedMessages.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void When_registered()
+            {
+                CompareResult compareResult = new();
+
+                var message1 = "message1";
+                var message2 = "message2";
+                compareResult.AddMismatchedMessage(message1);
+                compareResult.AddMismatchedMessage(message2);
+
+
+                compareResult.HasMismatched
+                    .Should().BeTrue();
+                compareResult.MismatchedMessages
+                    .Should().Equal(message1, message2);
+
+            }
+        }
+    }
+}
