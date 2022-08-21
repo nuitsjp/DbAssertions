e041f0c272be8f8176226ee0e94c7f4aaaa29289
Atsushi Nakamura <nuits.jp@live.jp>
2022-08-22 05:38:37 +0900

    Update solution.

--- Sample/PowerShell/Invoke-Second.ps1
+++ Sample/PowerShell/Invoke-Second.ps1
@@ -2,7 +2,7 @@ $server                 = 'localhost'
 $database               = 'AdventureWorks'
 $user                   = 'sa'
 $password               = 'P@ssw0rd!'
-$initializedDateTime    = '2022-08-20 23:01:11.780'
+$initializedDateTime    = '2022-08-21 11:30:33.807'
 $output                 = '..\bin\Debug\Expected'
 
 # second -s localhost -d AdventureWorks -u sa -p P@ssw0rd! -o Expected -i "2022-08-20 22:53:28.623"
