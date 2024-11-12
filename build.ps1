7033bd9528689749cb5098238d5ca6a0c1b2275a
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-11-12 15:27:49 +0900

    ターゲットフレームワークをnet8.0に更新し、ビルドスクリプトを修正

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ Param(
     [string] $Version
 )
 
-dotnet publish .\DbAssertions.SqlServer.App\DbAssertions.SqlServer.App.csproj -c Release -r win10-x64 -f net6.0 /p:Version=$Version --self-contained=false
-dotnet publish .\DbAssertions.SqlServer.App\DbAssertions.SqlServer.App.csproj -c Release -r win10-x64 -f net48 /p:Version=$Version
+dotnet publish .\DbAssertions.SqlServer.App\DbAssertions.SqlServer.App.csproj -c Release -r win-x64 -f net8.0 /p:Version=$Version --self-contained=false
+dotnet publish .\DbAssertions.SqlServer.App\DbAssertions.SqlServer.App.csproj -c Release -r win-x64 -f net48 /p:Version=$Version
